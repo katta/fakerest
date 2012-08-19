f6185f099ee85359b9af82264f2d75b51eb7ce13
Srivatsa Katta <vatsa.katta@gmail.com>
2012-08-19 20:08:35 +0530

    removed shell scripts

--- restartstubs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kill -9 $(ps -ef | awk '/[r]uby stubs.rb/{ print $2 }')
-ruby stubs.rb profiles/kilkari.profile &> /var/log/kilkari/stubs.log &
-echo "Logs can be found @ /var/log/kilkari/stubs.log"
