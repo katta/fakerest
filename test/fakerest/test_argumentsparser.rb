143165f7fe75c799c45b47a962e3abc0278bdf47
Srivatsa Katta <vatsa.katta@gmail.com>
2012-08-22 20:17:55 +0530

    Added tests to check port argument

--- test/fakerest/test_argumentsparser.rb
+++ test/fakerest/test_argumentsparser.rb
@@ -2,10 +2,26 @@ require 'test/unit'
 require 'fakerest/argumentsparser'
 
 class ArgumentsParserTest < Test::Unit::TestCase
+  
+  def setup
+    @parser = FakeRest::ArgumentsParser.new
+  end
+  
   def test_should_check_for_mandatory_config_argument
-    parser = FakeRest::ArgumentsParser.new
-    options = parser.parse(["-c","config"])
+    options = @parser.parse(["-c","config"])
 
     assert_equal("config", options[:config], "Expected config argument but not found.")
+  end
+
+  def test_default_port_is_1111
+    options = @parser.parse(["-c" , "config"])
+
+    assert_equal(1111, options[:port], "Expected default port to be 1111")
+  end
+  
+  def test_should_override_default_port
+    options = @parser.parse(["-c" , "config", "-p", "2222"])
+
+    assert_equal("2222", options[:port] )
   end
 end
